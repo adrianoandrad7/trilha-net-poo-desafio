4999e950288d868eb1e72d73690c81afd91ef000
adriano andrade <adrianoandraden14@hotmail.com>
2023-11-05 23:53:38 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone iPhone");
+Iphone ip = new Iphone("165525632", "15 PRO","8862", 128);
+ip.Ligar();
+ip.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia");
+Nokia nk = new Nokia("88563279", "Nokia T300", "8974", 64);
+nk.ReceberLigacao();
+nk.InstalarAplicativo("Instagram");
+
+
